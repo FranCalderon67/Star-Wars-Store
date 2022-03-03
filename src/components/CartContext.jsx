bf9c3eb35f414aeb89df5bbfc7c86e4f3964fb9f
Francisco Calderon <franciscocalderon1990@gmail.com>
2022-03-03 20:23:40 -0300

    Eliminar Item

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -7,6 +7,20 @@ function CartProvider({ children }) {
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
 
+  // const addItem = (item, quantity) => {
+  //   if (isInCart(item.id)) {
+  //     const newCart = [...cartItems];
+  //     for (const it of newCart) {
+  //       if (it.item.id == item.id) {
+  //         it.quantity = it.quantity + quantity;
+  //       }
+  //     }
+  //     setCartItems(newCart);
+  //   } else {
+  //     setCartItems([...cartItems, { item: item, quantity: quantity }]);
+  //   }
+  // };
+
   const addItem = (item, cantidad) => {
     if (cartItems.some((producto) => producto.id == item.id)) {
       const copy = [...cartItems];
@@ -21,17 +35,20 @@ function CartProvider({ children }) {
     }
   };
 
-  // const vaciarCarrito = (item) => {
-  //   cartItems.splice((producto) => producto.id !== item.id);
-  //   setCartCount(0);
-  // };
-
   const vaciarCarrito = () => {
     setCartItems([]);
     setCartCount(0);
   };
+  // const isInCart = (id) => {
+  //   return cartItems.find((e) => e.item.id === id);
+  // };
 
-  const eliminarItem = (item) => {};
+  const eliminarItem = (id) => {
+    const nuevoCarrito = [...cartItems].map((item) => item.id !== id);
+    setCartItems(nuevoCarrito);
+    // setCartCount((prev) => prev - );
+    console.log(nuevoCarrito);
+  };
 
   return <CartContext.Provider value={{ cartItems, cartCount, addItem, eliminarItem, vaciarCarrito }}>{children}</CartContext.Provider>;
 }
